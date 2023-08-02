6669d3bf4da9c1a938932de690d80337945279cf
Mukhutdinov Ruslan <mukhutdinov.ruslan@gmail.com>
2023-08-02 12:20:45 +0500

    add check platform

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted } from "vue";
+import { Capacitor } from "@capacitor/core";
+
+onMounted(() => {
+  alert(Capacitor.getPlatform());
+});
 </script>
 
 <template>
